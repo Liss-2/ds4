6191a8088d75ebadcb6f0cc0f2c16003a158e0ae
Liseth Montezuma <liseth.montezuma@utp.ac.pa>
2024-09-16 19:07:22 -0500

    Excepciones utilizando finally (ejecutar después del bloque try/catch)

---
+++ Laboratorio6/Laboratorio63/Program.cs
@@ -0,0 +1,29 @@
+﻿/*internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}*/
+
+class Program
+{
+
+    static void main(String[] args)
+    {
+        try
+        {
+            int[] myNumbers = { 1, 2, 3 };
+            Console.WriteLine(myNumbers[10]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Algo salió mal, valide el indice del arreglo");
+        }
+        finally
+        {
+
+            Console.WriteLine("Continuacion de la aplicacion, luego del bloque try/catch");
+        }
+    }
+}
