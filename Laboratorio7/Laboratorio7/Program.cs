8e4680d04b351ed0e7ae5703160182de91b882d3
Liseth Montezuma <liseth.montezuma@utp.ac.pa>
2024-09-16 19:29:36 -0500

    Ejemplo 71

---
+++ Laboratorio7/Laboratorio7/Program.cs
@@ -0,0 +1,12 @@
+﻿/*internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}*/
+
+static void main (String[] args)
+{
+   
+}
