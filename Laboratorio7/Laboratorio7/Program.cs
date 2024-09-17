6c58c59320002b955c8781584196a8674e184c5f
Liseth Montezuma <liseth.montezuma@utp.ac.pa>
2024-09-16 19:56:54 -0500

    Codificación del método principal

--- Laboratorio7/Laboratorio7/Program.cs
+++ Laboratorio7/Laboratorio7/Program.cs
@@ -6,7 +6,12 @@
     }
 }*/
 
+using Laboratorio7;
+
 static void main (String[] args)
 {
-   
+   Banco banco1 = new Banco();
+    banco1.Operar();
+    banco1.DepositosTotales();
+    Console.ReadKey();
 }
