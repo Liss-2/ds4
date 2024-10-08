47796f45d5eda26f185652e7120a8f3d0f3c9cd9
Liseth Montezuma <liseth.montezuma@utp.ac.pa>
2024-10-08 01:30:37 -0500

    corregí el Console.WriteLine

--- Laboratorio2/laboratorio2/laboratorio2/Program.cs
+++ Laboratorio2/laboratorio2/laboratorio2/Program.cs
@@ -1,122 +1,8 @@
 ﻿
-/*internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-
-    } 
-    //ejemplo de variable local 1
-    public void Sumar(int valor1, int valor2)
-        {
-            int variableLocal = valor1 + valor2;
-            Console.WriteLine(variableLocal);   
-        }
-    
-}
-
-// --------------------------------------------------------------------------------------------------
-
-
-namespace Laboratorio2
-{
-    class Program
-    {
-
-        static void Main(string[] args)
-        {
-            Client client = new Client();
-            client.FirstName = "Su Nombre";
-            client.LastName = "Su Apellido";
-            client.Age = 15;
-            client.Id = 1;
-
-            Console.WriteLine(client.GetFullName());
-        }
-
-        public class Client
-        {
-            public int Id { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public ushort Age { get; set; }
-
-            public string GetFullName()
-            {
-                return FirstName + " " + LastName;
-            }
-        }
-    }
-}
-
-//----------------------------------------------------------------------------------------------------
-
-
-
-namespace Laboratorio21
-{
-    public class MyClass
-    {
-        //Declarando variable estática
-        public static int valor;
-    }
-
-    public class Program
-    {
-        public static void Main()
-        {
-
-            //asignando valor a variable estática
-            MyClass.valor = 1;
-            Console.WriteLine(MyClass.valor);
-        }
-
-    }
-
-}
-
-//------------------------------------------------------------------------------------
-
-
-internal class Program1
-{
-    //imprimiendo en consola variables
-    private static void Main(string[] args)
-    {
-        int valor1 = 28;
-        int valor2 = valor1;
-        valor2 = 30;
-        Console.WriteLine(valor1);
-        Console.WriteLine(valor2);
-        
-        Console.WriteLine(valor1);
-        Console.WriteLine(valor2);
-    }
-}
-
-//----------------------------------------------------------------------------------------
-
-internal class Program2
-{
-    private static void Main(string[] args)
-    {
-        int valor1 = 28;
-        int valor2 = valor1;
-        valor2 = 30;
-        Console.WriteLine(valor1);
-        Console.WriteLine(valor2);
-
-        Console.WriteLine(valor1);
-        Console.WriteLine(valor2);
-    }
-}
-
-//*************************************************************************************************************
-*/
 
 namespace Laboratorio22
 {
-    public class MyClass
+    class MyClass
     {
         //Declarando variable estática
         public string Nombre { get; set; }
@@ -141,8 +27,8 @@ namespace Laboratorio22
             object2.Nombre = "Jose";
 
             //Al imprimir en consola vemos que ambas referencias imprimen el mismo valor 'Jose'
-            Console.WriteLine(object2);
-            Console.WriteLine(object1);
+            Console.WriteLine(object2.Nombre);
+            Console.WriteLine(object1.Nombre);
 
         }
 
