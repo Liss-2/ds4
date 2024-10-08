5f92776408f664656984108bfc6bc9f699b46de6
Liseth Montezuma <liseth.montezuma@utp.ac.pa>
2024-10-08 02:41:52 -0500

    Laboratorio47 completo

---
+++ Laboratorio4/Laboratorio47/Program.cs
@@ -0,0 +1,31 @@
+﻿/*internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}*/
+
+namespace Lab47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUno = 70;
+            double numeroDos = 67.89;
+            double numeroTres = 67.89;
+
+            Console.WriteLine(Suma(numeroUno, numeroDos));
+            Console.WriteLine(Suma(numeroUno, numeroDos, numeroTres));
+            
+            //compare y analice las salidas de las 2 sumas realizadas
+
+        }
+        static double Suma(int x, double y, double z= 0)
+        {
+            return x + y + z;
+        }
+    }
+
+}
