72c8edf1f5dad9a05fe5e4e23d36bb7d186b4008
Liseth <liss.eml027@gmail.com>
2025-09-17 17:54:47 -0500

    Creación del método Sumar()

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+
+    //Definición de método que suma dos números e imprime el resultado.
+    public void Sumar(int valor1, int valor2)
+    {
+        int variableLocal = valor1 + valor2;//declarndo variable local variableLocal
+        Console.WriteLine(variableLocal);//Esto imprime el resultado de la suma
+    }
+    
+}
