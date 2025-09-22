28bb2cb3babce227f362598d57176921f1abe7d0
Liss-2 <lisethml73@gmail.com>
2025-09-22 16:29:23 -0500

    lab46 listo

---
+++ Laboratorio4/Laboratorio4/Laboratorio46.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio4
+{
+    internal class Laboratorio46
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el radio del circulo");
+            double radio = double.Parse(Console.ReadLine());
+
+            double area = Math.Pow(radio, 2) * Math.PI;
+
+            Console.WriteLine($"El area del circulo es: {area}");
+        }
+    }
+}
--- Laboratorio4/Laboratorio4/obj/Debug/net8.0/Laboratorio4.AssemblyInfo.cs
+++ Laboratorio4/Laboratorio4/obj/Debug/net8.0/Laboratorio4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6041b499800c568acb8c1b98ec6f517ab4a9e140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97c49710056715e4bc56a41baf5366c88bcaa783")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
