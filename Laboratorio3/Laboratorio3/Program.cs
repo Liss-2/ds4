29f44d7dbb731c785bd19f91cb7707d6d5629bab
Liseth <liss.eml027@gmail.com>
2025-09-22 12:24:21 -0500

    ejemplo 1

---
+++ Laboratorio3/Laboratorio3/Program.cs
@@ -0,0 +1,22 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // ------- ejemplo 1 --------
+        int primerNumero, segundoNumero, suma;
+
+        Console.WriteLine("Introduce el primer numero: ");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo numero: ");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        suma = primerNumero + segundoNumero;
+
+        Console.WriteLine("La suma de {0} y {1} es {2}", primerNumero, segundoNumero, suma);
+
+
+    }
+}
