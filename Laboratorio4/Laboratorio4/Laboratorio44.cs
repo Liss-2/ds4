6041b499800c568acb8c1b98ec6f517ab4a9e140
Liseth <liss.eml027@gmail.com>
2025-09-22 14:28:28 -0500

    lab44 listo

---
+++ Laboratorio4/Laboratorio4/Laboratorio44.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio4
+{
+    internal class Laboratorio44
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la nota del estudiante");
+            float score = float.Parse(Console.ReadLine());
+            if (score >= 70)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Su nota es {score} ha aprobado");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Su nota es {score} ha reprobado, debe repetir");
+            }
+        }
+    }
+}
--- Laboratorio4/Laboratorio4/obj/Debug/net8.0/Laboratorio4.AssemblyInfo.cs
+++ Laboratorio4/Laboratorio4/obj/Debug/net8.0/Laboratorio4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72559071e73d7df4ca35a5014ef6e6a6b024d2f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+313514272ef03a97e9627591df94e64600ea427e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
