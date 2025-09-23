51398bbfcd2289b47767f1aea4f546c778d3d468
Liss-2 <lisethml73@gmail.com>
2025-09-22 21:04:50 -0500

    lab64 listo

---
+++ Laboratorio6/Laboratorio6/Laboratorio64.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio6
+{
+    internal class Laboratorio64
+    {
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException(message: "Acceso negado - No cumple con el criterio de edad");
+            }
+            else
+            {
+                Console.WriteLine("Acceso Concedido");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            checkAge(15);
+        }
+    }
+}
--- Laboratorio6/Laboratorio6/obj/Debug/net8.0/Laboratorio6.AssemblyInfo.cs
+++ Laboratorio6/Laboratorio6/obj/Debug/net8.0/Laboratorio6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aad6b6eed5955c7d6d12d56f9afc6a06f799526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19078ba5c401ce272153212d62067e5ed5d5ef4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
