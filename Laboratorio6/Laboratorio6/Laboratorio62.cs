4aad6b6eed5955c7d6d12d56f9afc6a06f799526
Liss-2 <lisethml73@gmail.com>
2025-09-22 20:47:50 -0500

    lab62 -> comentario

--- Laboratorio6/Laboratorio6/Laboratorio62.cs
+++ Laboratorio6/Laboratorio6/Laboratorio62.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
 namespace Laboratorio6
 {
     internal class Laboratorio62
@@ -30,4 +30,4 @@ namespace Laboratorio6
 
         }
     }
-}
+}*/
