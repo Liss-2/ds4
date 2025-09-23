15c38080ad67816ab7af39a109a9a16e2f27854c
Liss-2 <lisethml73@gmail.com>
2025-09-22 20:17:58 -0500

    lab55 -> comentario

--- Laboratorio5/Laboratorio5/Laboratorio55.cs
+++ Laboratorio5/Laboratorio5/Laboratorio55.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
 namespace Laboratorio5
 {
     internal class Laboratorio55
@@ -30,3 +30,4 @@ namespace Laboratorio5
         }
     }
 }
+*/
