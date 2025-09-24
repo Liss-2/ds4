ef77741360b0b3dac45046e22cd1b4ba02ab7d70
Liseth <liss.eml027@gmail.com>
2025-09-24 13:31:58 -0500

    creación del lab

---
+++ Laboratorio4/Laboratorio4/Laboratorio45.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio4
+{
+    internal class Laboratorio45
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables
+            int edad1;
+            int edad2;
+            int edad3;
+            int edad4;
+            int edad5 = 31;
+
+            //Inicializacion de variables
+            edad1 = edad2 = edad3 = edad4 = 32;
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", edad1, edad2, edad3, edad4, edad5);
+        }
+    }
+}
--- Laboratorio7/Laboratorio7/obj/Debug/net8.0/Laboratorio7.AssemblyInfo.cs
+++ Laboratorio7/Laboratorio7/obj/Debug/net8.0/Laboratorio7.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio4")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6041b499800c568acb8c1b98ec6f517ab4a9e140")]
-[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio4")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio4")]
+[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio7")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
