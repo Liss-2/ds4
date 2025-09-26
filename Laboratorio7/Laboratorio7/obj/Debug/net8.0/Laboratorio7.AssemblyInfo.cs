c48fe993c06ed053dbb2ac478606863987970e41
Liseth <liss.eml027@gmail.com>
2025-09-25 19:13:32 -0500

    creación de clase cliente y banco

--- Laboratorio7/Laboratorio7/obj/Debug/net8.0/Laboratorio7.AssemblyInfo.cs
+++ Laboratorio7/Laboratorio7/obj/Debug/net8.0/Laboratorio7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc93760ee1951189dc9ebbb51d1eb039f3d7bcb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a22446bb8710b8b54afd0d11341dcd47a3f03d8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
