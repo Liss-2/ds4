882b1acefb8eda968c0cf2b9d4052f6fdad4e7bd
Liss-2 <lisethml73@gmail.com>
2025-09-30 11:29:15 -0500

    compilado con éxito

--- Laboratorio8/Laboratorio8/obj/Debug/net8.0/Laboratorio8.AssemblyInfo.cs
+++ Laboratorio8/Laboratorio8/obj/Debug/net8.0/Laboratorio8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55eb6333edb14d5df9e692b182f1d4c4ca40168b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd8ca8725670f5c508f9d7656476ca040298dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
