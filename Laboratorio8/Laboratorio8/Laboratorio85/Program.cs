60d7f605fe69cdf1013bc8d4da775e12b1fcc388
Liss-2 <lisethml73@gmail.com>
2025-10-02 00:23:09 -0500

    lab85 -> comentario

--- Laboratorio8/Laboratorio8/Laboratorio85/Program.cs
+++ Laboratorio8/Laboratorio8/Laboratorio85/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
 namespace Laboratorio8.Laboratorio85
 {
     internal class Program
@@ -14,4 +14,4 @@ namespace Laboratorio8.Laboratorio85
             misCoords.VerCoordenadas();
         }
     }
-}
+}*/
