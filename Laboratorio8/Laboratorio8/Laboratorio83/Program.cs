551e47c6e03742c73002e30bceaaedbcdc235077
Liseth <liss.eml027@gmail.com>
2025-10-01 22:26:17 -0500

    creación de carpeta lab83

---
+++ Laboratorio8/Laboratorio8/Laboratorio83/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8.Laboratorio83
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Suma(1.0, 2.2));
+        }
+
+        static int Suma(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double Suma(double x, double y)
+        {
+            return x + y;
+        }
+
+        static long Suma(long x, long y)
+        {
+            return x + y;
+        }
+    }
+}
