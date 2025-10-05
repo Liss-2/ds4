8e0e205652c0b29804074b5127594e9271fb8e83
Liss-2 <lisethml73@gmail.com>
2025-10-04 20:24:13 -0500

    lab87 -> comentario

--- Laboratorio8/Laboratorio8/Laboratorio87/ClaseBase.cs
+++ Laboratorio8/Laboratorio8/Laboratorio87/ClaseBase.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
 namespace Laboratorio8.Laboratorio87
 {
     internal class ClaseBase
@@ -17,4 +17,4 @@ namespace Laboratorio8.Laboratorio87
             Console.WriteLine("Se ejecutó: Metodo MasTest en ClaseBase");
         }
     }
-}
+}*/
