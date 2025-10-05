42ffeaa8f2053f1c4cac6800fa1e4ed661f28eb6
Liss-2 <lisethml73@gmail.com>
2025-10-04 21:56:26 -0500

    class Template creada

---
+++ Laboratorio8/Laboratorio8/Laboratorio89/Template.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8.Laboratorio89
+{
+    internal class Template : iTemplate
+    {
+        public void ponerVariable(string nombre, string var)
+        {
+            Console.WriteLine($"Metodo poner varibale {nombre} : {var}");
+        }
+
+        public void verHtml(string template)
+        {
+            Console.WriteLine($"Metodo ver html {template}");
+        }
+    }
+}
--- Laboratorio8/Laboratorio8/Laboratorio89/iTemplate.cs
+++ Laboratorio8/Laboratorio8/Laboratorio89/iTemplate.cs
@@ -8,5 +8,7 @@ namespace Laboratorio8.Laboratorio89
 {
     internal interface iTemplate
     {
+        public void ponerVariable(string nombre, string var);
+        public void verHtml(string template);
     }
 }
