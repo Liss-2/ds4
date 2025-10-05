3257df1fa2ef3b7ede2d211fbd1a0f7719c50123
Liss-2 <lisethml73@gmail.com>
2025-10-04 20:23:14 -0500

    lab87 listo

---
+++ Laboratorio8/Laboratorio8/Laboratorio87/ClaseHijo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio8.Laboratorio87
+{
+    internal class ClaseHijo : ClaseBase
+    {
+        public new void MasTest()
+        {
+            // Se oculta el metodo de la clase base
+            Console.WriteLine("Se ejecutó: Metodo oculto en ClaseHijo");
+        }
+    }
+}
