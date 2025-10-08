cd1e9da5a8dedffc1d1267c2d10861ae1c5cee87
Liseth <liss.eml027@gmail.com>
2025-10-07 22:45:16 -0500

    Laboratorio10 completo

---
+++ Laboratorio10/Laboratorio10/Laboratorio101.vb
@@ -0,0 +1,10 @@
+﻿Module Laboratorio101
+
+    Sub Main()
+
+        Console.WriteLine("Hola")
+        Console.WriteLine("Este es mi primer programa de consola")
+
+    End Sub
+
+End Module
---
+++ Laboratorio10/Laboratorio10/Laboratorio102.vb
@@ -0,0 +1,21 @@
+﻿Module Laboratorio102
+
+    Sub main()
+        Dim radio As Single
+        Dim area As Single
+        Dim circunferencia As Single
+        Const pi = 3.1415926
+
+        Console.Write("Ingrese el radio: ")
+        radio = Console.ReadLine
+
+        area = pi * radio ^ 2
+        circunferencia = 2 * pi * radio
+
+        Console.WriteLine("El area es: {0}", area)
+        Console.WriteLine("La circungerencia es: {0}", circunferencia)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
